fb4c45de47fee7631665d95fac4ddcb55345451d
Esther Chan <estherchanleehsuen@gmail.com>
2025-03-04 08:02:05 +0800

    events.js update

--- resources/js/admin/events.js
+++ resources/js/admin/events.js
@@ -14,40 +14,4 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    // Notification Bell Functionality
-    const notificationBell = document.getElementById("notificationBell");
-    const notificationDropdown = document.getElementById("notificationDropdown");
-
-    if (notificationBell) {
-        notificationBell.addEventListener("click", function () {
-            fetch("/notifications")
-                .then(response => response.json())
-                .then(data => {
-                    let notifications = "";
-                    if (data.length > 0) {
-                        data.forEach(notification => {
-                            notifications += `
-                                <li class="notification-item">
-                                    <a href="#" class="notification-link" data-id="${notification.id}">
-                                        ${notification.message}
-                                    </a>
-                                </li>`;
-                        });
-                    } else {
-                        notifications = '<li class="notification-item">No new notifications</li>';
-                    }
-
-                    notificationDropdown.innerHTML = notifications;
-
-                    // Mark as read when clicked
-                    document.querySelectorAll(".notification-link").forEach(link => {
-                        link.addEventListener("click", function () {
-                            let notificationId = this.dataset.id;
-                            fetch(`/notifications/${notificationId}/read`, { method: "POST" })
-                                .then(() => this.parentElement.remove()); 
-                        });
-                    });
-                });
-        });
-    }
 });
